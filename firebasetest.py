a90e6cee1f18cc9cd03aed10a0a908f30d7f4b1f
mati22124 <65964806+mati22124@users.noreply.github.com>
2024-02-19 20:04:05 -0500

    final project for storing grades. without other calulations

--- firebasetest.py
+++ firebasetest.py
@@ -31,34 +31,49 @@ while True:
 print("""
 1. print classes w/ grade
 2. print classes w/o grade
-2. add class
-3. add assignment
-5. remove class
-6. remove grade
-7. calculate grade needed for a _
-8. calulate minimum grade needed to keep your current grade.
+3. print assignments for a certain class
+4. add class
+5. add assignment
+6. remove class
+7. remove assignment
+8. calculate grade needed for a _
+9. calulate minimum grade needed to keep your current grade.
+0. save and exit
+
+enter your choice here (number): 
 """)
 
-# data = {
-#   "classes": [ #array of classes
-#     { #start of first class
-#       "name": "class1",
-#       "assignments": [ #array of assignments
-#         { #start of first assignment
-#           "name": "assignment 1",
-#           "pointsHave": 2,
-#           "pointsOutOf": 2,
-#           "Category": "Unweighted",
-#           "Weight": 1
-#         },
-#         { #start of 2nd assignemnt
-#           "name": "assignment 2",
-#           "pointsHave": 5,
-#           "pointsOutOf": 5,
-#           "Category": "Unweighted",
-#           "Weight": 1 
-#         }
-#       ]
-#     }
-#   ]
-# }
+
+while True:
+    x = input()
+    if x == "0":
+        school.setInfo()
+        print("information saved. thanks for using")
+        break
+    if x == "1":
+        school.getClasses(True)
+    if x == "2":
+        school.getClasses()
+    if x == "3":
+        school.getClasses()
+        y = int(input("what is the class number? "))
+        school.getAssignments(y)
+    if x == "4":
+        name = input("Enter the class name: ")
+        school.addClass(name)
+    if x == "5":
+        school.getClasses()
+        y = int(input("what is the class number? "))
+        name = input("Assignment name: ")
+        poo = int(input("How many points is it out of: "))
+        ph = int(input("How many points did you get: "))
+        school.addAssignment(y, name, ph, poo)
+    if x == "6":
+        y = int(input("what is the class number? "))
+        school.removeClass(y)
+    if x == "7":
+        school.getClasses()
+        y = int(input("what is the class number? "))
+        school.getAssignments(y)
+        z = int(input("what is the assignment number? "))
+        school.removeAssignment(y, z)
--- grades.py
+++ grades.py
@@ -60,6 +60,21 @@ class grade:
         except:
             print("error smth happened wrong")
 
+    def getClasses(self, withGrade = False):
+        x = 0
+        for i in self.data:
+            if withGrade:
+                print(str(x)+": "+i["name"]+" - "+str(self.getGrade(x))+" - "+self.getLetterGrade(x))
+            else:
+                print(str(x)+": "+i["name"])
+            x+=1
+
+    def getAssignments(self, classId):
+        x = 0
+        for i in self.data[classId]["assignments"]:
+            print(str(x)+": "+i["name"]+" - "+str(i["pointsHave"])+"/"+str(i["pointsOutOf"]) + " - " + str(i["pointsHave"]*100/i["pointsOutOf"])+"%") #add 0 handling later
+            x+=1
+
     def sumPointsHave(self, classId):
         sum = 0
         for i in self.data[classId]["assignments"]:
@@ -78,3 +93,49 @@ class grade:
         else:
             return 0
     
+    def getLetterGrade(self, classId):
+        x = self.getGrade(classId)
+        if x>=93:
+            return "A"
+        elif x>=90:
+            return "A-"
+        elif x>=87:
+            return "B+"
+        elif x>=83:
+            return "B"
+        elif x>=80:
+            return "B-"
+        elif x>=77:
+            return "C+"
+        elif x>=73:
+            return "C"
+        elif x>=70:
+            return "C-"
+        elif x>=67:
+            return "D+"
+        elif x>=63:
+            return "D"
+        elif x>=60:
+            return "D-"
+        else:
+            return "F"
+
+    def removeClass(self, classId):
+        x = self.data.pop(classId)
+        print("Would you like to remove "+ x["name"]+ "from your gradebook? (y/n)")
+        y = input()
+        if y != "y":
+            self.data.append(x)
+            print("ok, item not removed.")
+        else:
+            print("done")
+    
+    def removeAssignment(self, classId, assignmentId):
+        x = self.data[classId]["assignments"].pop(assignmentId)
+        print("Would you like to remove \""+ x["name"]+ "\" from your gradebook? (y/n)")
+        y = input()
+        if y != "y":
+            self.data[classId]["assignments"].append(x)
+            print("ok, item not removed.")
+        else:
+            print("done")
